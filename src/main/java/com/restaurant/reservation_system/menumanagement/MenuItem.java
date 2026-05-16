d236e9bf00f61a5254b431961707e58f3af60bda
IT25100977 <IT25100977@my.sliit.lk>
2026-05-17 00:30:50 +0530

    Add getters and setters

--- src/main/java/com/restaurant/reservation_system/menumanagement/MenuItem.java
+++ src/main/java/com/restaurant/reservation_system/menumanagement/MenuItem.java
@@ -17,4 +17,53 @@ public class MenuItem {
     private double price;
     private String description;
     private boolean available = true;
+
+    //getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
