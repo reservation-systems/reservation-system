2d5e95ca24a2a3fc1b745c2cc4f07f3fa4f56a3f
IT25100977 <IT25100977@my.sliit.lk>
2026-05-17 00:27:47 +0530

    Create attributes

--- src/main/java/com/restaurant/reservation_system/menumanagement/MenuItem.java
+++ src/main/java/com/restaurant/reservation_system/menumanagement/MenuItem.java
@@ -3,5 +3,18 @@ package com.restaurant.reservation_system.menumanagement;
 //library use to connect java classes with database tables
 import jakarta.persistence.*;
 
+//entire class should behave as a database
+@Entity
+
 public class MenuItem {
+    @Id //marks primary key
+    //automatically generate IDs and database auto increments values
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private double price;
+    private String description;
+    private boolean available = true;
 }
