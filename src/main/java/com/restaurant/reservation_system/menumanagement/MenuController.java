2bfbb9282abc78f55449cdc0aab2357d008ae22c
IT25100977 <IT25100977@my.sliit.lk>
2026-05-17 01:01:44 +0530

    Add MenuController

---
+++ src/main/java/com/restaurant/reservation_system/menumanagement/MenuController.java
@@ -0,0 +1,9 @@
+package com.restaurant.reservation_system.menumanagement;
+
+//importing all notations from Spring's web package
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+public class MenuController {
+
+}
