a737e6d58bbf728d63f22bb12587069d1a6ad357
IT25100977 <IT25100977@my.sliit.lk>
2026-05-17 20:46:35 +0530

    Add getters and setters

--- src/main/java/com/restaurant/reservation_system/menumanagement/PreOrder.java
+++ src/main/java/com/restaurant/reservation_system/menumanagement/PreOrder.java
@@ -17,5 +17,81 @@ public class PreOrder {
     private int quantity;
     private double totalPrice;
     private LocalDateTime orderTime;
-    private String status = "PENDING";
+    private String status = "PENDING"; //order status set as "Pending" until processed
+
+    @ManyToOne //many PreOrders can belong to one MenuItem
+    @JoinColumn(name = "menu_item_id") //define foreign key column in PreOrder table
+    private MenuItem menuItem;
+
+    //automatically runs before saving the entity to database
+    @PrePersist
+    public void onCreate() {
+        orderTime = LocalDateTime.now();
+    }
+
+    //getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
+    public void setOrderTime(LocalDateTime orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+    public void setMenuItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+    }
 }
