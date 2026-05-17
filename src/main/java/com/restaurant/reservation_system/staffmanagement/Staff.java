5ab57f148b9e17ac3ff0b6e2a3652482d5a9adf9
IT25102606 <it25102606@my.sliit.lk>
2026-05-17 22:46:59 +0530

    Add Getter & Setter

--- src/main/java/com/restaurant/reservation_system/staffmanagement/Staff.java
+++ src/main/java/com/restaurant/reservation_system/staffmanagement/Staff.java
@@ -13,4 +13,53 @@ public class Staff {
      private String phone;
      private String email;
      private double salary;
+
+     //Getter & Setter
+     public Long getId() {
+          return id;
+     }
+
+     public void setId(Long id) {
+          this.id = id;
+     }
+
+     public String getRole() {
+          return role;
+     }
+
+     public void setRole(String role) {
+          this.role = role;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public String getPhone() {
+          return phone;
+     }
+
+     public void setPhone(String phone) {
+          this.phone = phone;
+     }
+
+     public String getEmail() {
+          return email;
+     }
+
+     public void setEmail(String email) {
+          this.email = email;
+     }
+
+     public double getSalary() {
+          return salary;
+     }
+
+     public void setSalary(double salary) {
+          this.salary = salary;
+     }
 }
