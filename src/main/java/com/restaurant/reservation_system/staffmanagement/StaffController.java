c0282bf0bee0b46b6c79c89a41ae6273d5befd93
IT25102606 <it25102606@my.sliit.lk>
2026-05-18 00:52:20 +0530

    Create StaffController class

--- src/main/java/com/restaurant/reservation_system/staffmanagement/StaffController.java
+++ src/main/java/com/restaurant/reservation_system/staffmanagement/StaffController.java
@@ -3,6 +3,9 @@ package com.restaurant.reservation_system.staffmanagement;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/staff")
+@CrossOrigin("*")
 public class StaffController {
 
 }
