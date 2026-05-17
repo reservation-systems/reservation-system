4782e3525eab722deee136c43c35112764a59f5e
IT25100010 <IT25100010@my.sliit.lk>
2026-05-18 00:29:07 +0530

    Create basic admin entity

--- src/main/java/com/restaurant/reservation_system/adminmanagement/Admin.java
+++ src/main/java/com/restaurant/reservation_system/adminmanagement/Admin.java
@@ -1,4 +1,13 @@
 package com.restaurant.reservation_system.adminmanagement;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Admin {
+
+    @Id
+    private Long id;
+
+    private String name;
 }
