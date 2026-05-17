2757a6a92d68ca781b916c49d6ad00da20d308f3
IT25100977 <IT25100977@my.sliit.lk>
2026-05-17 15:13:46 +0530

    Add menu item repository

--- src/main/java/com/restaurant/reservation_system/menumanagement/MenuItemRepository.java
+++ src/main/java/com/restaurant/reservation_system/menumanagement/MenuItemRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+    List<MenuItem> findByAvailableTrue();
 }
