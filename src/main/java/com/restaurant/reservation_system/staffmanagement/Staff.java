d925df93d40c96c212ddf64568f1154add44d857
IT25102606 <it25102606@my.sliit.lk>
2026-05-17 19:35:41 +0530

    Add name field to Staff class

--- src/main/java/com/restaurant/reservation_system/staffmanagement/Staff.java
+++ src/main/java/com/restaurant/reservation_system/staffmanagement/Staff.java
@@ -1,8 +1,8 @@
 package com.restaurant.reservation_system.staffmanagement;
 
 public class Staff {
-     public String name;
-     public String role;// e.g. Chef, Waiter, Manager
+     private String name;
+
 
 
 }
