e89809a5d83ad793ddfbfc211f54915a0534e771
IT25102606 <it25102606@my.sliit.lk>
2026-05-17 22:17:05 +0530

    Add getters and setters to Staff class

--- src/main/java/com/restaurant/reservation_system/staffmanagement/Staff.java
+++ src/main/java/com/restaurant/reservation_system/staffmanagement/Staff.java
@@ -8,8 +8,52 @@ public class Staff {
      private double salary;
      private Long id;
 
+     // getter setter
+     public String getName() {
+          return name;
+     }
 
+     public void setName(String name) {
+          this.name = name;
+     }
 
+     public String getRole() {
+          return role;
+     }
 
+     public void setRole(String role) {
+          this.role = role;
+     }
 
+     public String getPhone() {
+          return phone;
+     }
+
+     public void setPhone(String phone) {
+          this.phone = phone;
+     }
+
+     public String getEmail() {
+          return email;
+     }
+
+     public void setEmail(String email) {
+          this.email = email;
+     }
+
+     public double getSalary() {
+          return salary;
+     }
+
+     public void setSalary(double salary) {
+          this.salary = salary;
+     }
+
+     public Long getId() {
+          return id;
+     }
+
+     public void setId(Long id) {
+          this.id = id;
+     }
 }
