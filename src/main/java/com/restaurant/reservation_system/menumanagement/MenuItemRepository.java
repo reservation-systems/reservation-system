3f42d450d7a7e85b47fc3b686bbd0ab97e0cd01b
IT25100977 <IT25100977@my.sliit.lk>
2026-05-17 16:02:38 +0530

    Add menu item repository

--- src/main/java/com/restaurant/reservation_system/menumanagement/MenuItemRepository.java
+++ src/main/java/com/restaurant/reservation_system/menumanagement/MenuItemRepository.java
@@ -3,6 +3,11 @@ package com.restaurant.reservation_system.menumanagement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
+//Repository interface for MenuItem entity
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+
+    //finds only menu items where "available" fields true
+    //Spring automatically creates the SQL query
+    //returns a list because multiple available menu items can exist
     List<MenuItem> findByAvailableTrue();
 }
