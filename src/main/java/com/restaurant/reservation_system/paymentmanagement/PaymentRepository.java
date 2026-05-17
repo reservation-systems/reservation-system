61ffde94c13063ee3371d8a90f383b1049af0eb9
senuth <it25102242@my.sliit.lk>
2026-05-17 21:56:53 +0530

    Add payment repository for data access

---
+++ src/main/java/com/restaurant/reservation_system/paymentmanagement/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.restaurant.reservation_system.paymentmanagement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByStatus(PaymentStatus status);
+    List<Payment> findByCustomerEmail(String customerEmail);
+}
