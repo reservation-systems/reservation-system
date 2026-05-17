6d3a869bc87a5c088d44ad2ddcb93b4bd5c5ca05
IT25101569 <it25101569@my.sliit.lk>
2026-05-17 19:04:09 +0530

    Class & Constructor creation

--- src/main/java/com/restaurant/reservation_system/reservationmanagement/Reservation.java
+++ src/main/java/com/restaurant/reservation_system/reservationmanagement/Reservation.java
@@ -1,6 +1,30 @@
 package com.restaurant.reservation_system.reservationmanagement;
 
+
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+@Entity
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+
+    private LocalDate reservationDate;
+    private LocalTime reservationTime;
+
+    private int numberOfGuests;
+
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status = ReservationStatus.PENDING;
 
 
 }
