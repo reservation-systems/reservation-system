7899d90330bd4bab86beefbb8ea38ca7921e0e32
IT25103627 <IT25103627@my.sliit.lk>
2026-05-17 18:17:40 +0530

    start

--- src/main/java/com/restaurant/reservation_system/reviewmanagement/Review.java
+++ src/main/java/com/restaurant/reservation_system/reviewmanagement/Review.java
@@ -1,7 +1,19 @@
 package com.restaurant.reservation_system.reviewmanagement;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Review {
-    public String test01;
-    public String test02;
-    public String test02223;
-}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String customerName;
+    private String customerEmail;
+
+    private int rating;
+    private String comment;
+
+    private LocalDateTime reviewDate;
