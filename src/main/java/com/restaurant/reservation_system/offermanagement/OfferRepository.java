b2ac286e38d0f5e643c36fbb6d0e648caa64ad2a
senuth <it25102242@my.sliit.lk>
2026-05-17 22:02:13 +0530

    Add offer repository and service layer

---
+++ src/main/java/com/restaurant/reservation_system/offermanagement/OfferRepository.java
@@ -0,0 +1,8 @@
+package com.restaurant.reservation_system.offermanagement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+    List<Offer> findByActiveTrue();
+}
