e48e1b5cdaee08d7f776b9e0033c823058937517
IT25100977 <IT25100977@my.sliit.lk>
2026-05-18 00:42:54 +0530

    set up PreOrderService structure and inject repositories

---
+++ src/main/java/com/restaurant/reservation_system/menumanagement/PreOrderService.java
@@ -0,0 +1,16 @@
+package com.restaurant.reservation_system.menumanagement;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+
+public class PreOrderService {
+    private final PreOrderRepository preOrderRepository;
+    private final MenuItemRepository menuItemRepository;
+
+    public PreOrderService(PreOrderRepository preOrderRepository, MenuItemRepository menuItemRepository) {
+        this.preOrderRepository = preOrderRepository;
+        this.menuItemRepository = menuItemRepository;
+    }
+
+}
