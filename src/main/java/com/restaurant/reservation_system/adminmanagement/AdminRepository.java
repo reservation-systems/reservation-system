7a81b468fbd0c2f3fa0e04f49353dd44550c592e
IT25100010 <IT25100010@my.sliit.lk>
2026-05-19 00:34:19 +0530

    update logib

--- src/main/java/com/restaurant/reservation_system/adminmanagement/AdminRepository.java
+++ src/main/java/com/restaurant/reservation_system/adminmanagement/AdminRepository.java
@@ -2,5 +2,17 @@ package com.restaurant.reservation_system.adminmanagement;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.List;
+
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+
+    Optional<Admin> findByEmail(String email);
+
+
+    boolean existsByEmail(String email);
+
+
+    List<Admin> findByRole(String role);
 }
