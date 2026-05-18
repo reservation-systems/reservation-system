e9e2fda8e350a92ad66b9d0a1c37d417cc43f02a
IT25100010 <IT25100010@my.sliit.lk>
2026-05-18 12:41:08 +0530

    add getters and setters

---
+++ src/main/java/com/restaurant/reservation_system/usermanagement/AuthController.java
@@ -0,0 +1,20 @@
+package com.restaurant.reservation_system.usermanagement;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin("*")
+public class AuthController {
+
+    private final AuthService authService;
+    private final CustomerService customerService;
+
+    public AuthController(AuthService authService,
+                          CustomerService customerService) {
+        this.authService = authService;
+        this.customerService = customerService;
+    }
+
+
+}
