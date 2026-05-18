0c5091754a92f24e79c2eda6f79210abcdcd47f8
IT25101569 <it25101569@my.sliit.lk>
2026-05-19 01:39:23 +0530

    Corrected code

--- src/main/java/com/restaurant/reservation_system/tablemanagement/RestaurantTableRepository.java
+++ src/main/java/com/restaurant/reservation_system/tablemanagement/RestaurantTableRepository.java
@@ -1,9 +1,13 @@
 package com.restaurant.reservation_system.tablemanagement;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long> {
+
     List<RestaurantTable> findByStatus(TableStatus status);
-    List<RestaurantTable> findByCapacityGreaterThanEqualAndStatus(int capacity, TableStatus status);
+
+    Optional<RestaurantTable> findByTableNumber(Integer tableNumber);
 }
