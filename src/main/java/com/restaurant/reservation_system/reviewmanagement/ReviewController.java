82862feb0d3351fb1284a5d32ac753c0620c18e2
IT25103627 <IT25103627@my.sliit.lk>
2026-05-18 10:26:25 +0530

    initialize ReviewController with endpoint mapping and dependency injection

--- src/main/java/com/restaurant/reservation_system/reviewmanagement/ReviewController.java
+++ src/main/java/com/restaurant/reservation_system/reviewmanagement/ReviewController.java
@@ -1,4 +1,15 @@
 package com.restaurant.reservation_system.reviewmanagement;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reviews")
+@CrossOrigin("*")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
 }
