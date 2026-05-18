26453e0a669f970d70cc2702c24e8a4c6cd3c135
IT25100010 <IT25100010@my.sliit.lk>
2026-05-19 01:05:17 +0530

    update

--- src/main/java/com/restaurant/reservation_system/usermanagement/Customer.java
+++ src/main/java/com/restaurant/reservation_system/usermanagement/Customer.java
@@ -1,9 +1,6 @@
 package com.restaurant.reservation_system.usermanagement;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Customer extends User {
