821f8ffc42a732b629bd2b741c9856b05d2bdf1f
IT25101569 <it25101569@my.sliit.lk>
2026-05-18 08:07:26 +0530

    Added standard getters and setters to implement the encapsulation for RestaurantTable

--- src/main/java/com/restaurant/reservation_system/tablemanagement/RestaurantTable.java
+++ src/main/java/com/restaurant/reservation_system/tablemanagement/RestaurantTable.java
@@ -14,4 +14,17 @@ public class RestaurantTable {
 
     @Enumerated(EnumType.STRING)
     private TableStatus status = TableStatus.AVAILABLE;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public int getTableNumber() { return tableNumber; }
+    public void setTableNumber(int tableNumber) { this.tableNumber = tableNumber; }
+
+    public int getCapacity() { return capacity; }
+    public void setCapacity(int capacity) { this.capacity = capacity; }
+
+    public TableStatus getStatus() { return status; }
+    public void setStatus(TableStatus status) { this.status = status; }
+
 }
