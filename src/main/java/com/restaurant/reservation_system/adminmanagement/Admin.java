7bb9fc08689dff9b0effe2661088fd1c1cf99729
IT25100010 <IT25100010@my.sliit.lk>
2026-05-19 00:34:19 +0530

    update logib

--- src/main/java/com/restaurant/reservation_system/adminmanagement/Admin.java
+++ src/main/java/com/restaurant/reservation_system/adminmanagement/Admin.java
@@ -3,6 +3,7 @@ package com.restaurant.reservation_system.adminmanagement;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "admins")
 public class Admin {
 
     @Id
@@ -10,19 +11,59 @@ public class Admin {
     private Long id;
 
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
 
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    private String role = "ADMIN";
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public Admin() {
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public Admin(String name, String email, String password, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
