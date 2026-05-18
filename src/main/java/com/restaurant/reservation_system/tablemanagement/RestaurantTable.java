ee5847f5dc93533a76a909241a105ea47e7f0b4e
IT25101569 <it25101569@my.sliit.lk>
2026-05-18 08:05:12 +0530

    Created RestaurantTable entity class with jpa annotations and database column definitions

--- src/main/java/com/restaurant/reservation_system/tablemanagement/RestaurantTable.java
+++ src/main/java/com/restaurant/reservation_system/tablemanagement/RestaurantTable.java
@@ -1,6 +1,17 @@
 package com.restaurant.reservation_system.tablemanagement;
 
+import jakarta.persistence.*;
+
+@Entity
 public class RestaurantTable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private int tableNumber;
+    private int capacity;
+
+    @Enumerated(EnumType.STRING)
+    private TableStatus status = TableStatus.AVAILABLE;
 }
