e3bd89865d7c45c950acdd1735534f3269f2e362
IT25100010 <IT25100010@my.sliit.lk>
2026-05-18 23:32:33 +0530

    add CustomerController with GET

---
+++ src/main/java/com/restaurant/reservation_system/usermanagement/CustomerController.java
@@ -0,0 +1,33 @@
+package com.restaurant.reservation_system.usermanagement;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+@CrossOrigin("*")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<Customer> getCustomerByEmail(@PathVariable String email) {
+        Customer customer = customerService.findByEmail(email);
+
+        if (customer == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(customer);
+    }
