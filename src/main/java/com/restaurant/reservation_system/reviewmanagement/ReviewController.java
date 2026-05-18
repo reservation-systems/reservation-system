4478d40b78a25303e168f4edfbe2fdf4a44a02cb
IT25103627 <IT25103627@my.sliit.lk>
2026-05-18 10:26:54 +0530

    expose GET endpoints for retrieving all reviews and filtering by customer email

--- src/main/java/com/restaurant/reservation_system/reviewmanagement/ReviewController.java
+++ src/main/java/com/restaurant/reservation_system/reviewmanagement/ReviewController.java
@@ -1,6 +1,7 @@
 package com.restaurant.reservation_system.reviewmanagement;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/reviews")
@@ -11,5 +12,15 @@ public class ReviewController {
 
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
+    }
+
+    @GetMapping
+    public List<Review> getAllReviews() {
+        return reviewService.getAllReviews();
+    }
+
+    @GetMapping("/customer")
+    public List<Review> getReviewsByCustomerEmail(@RequestParam String email) {
+        return reviewService.getReviewsByCustomerEmail(email);
     }
 }
