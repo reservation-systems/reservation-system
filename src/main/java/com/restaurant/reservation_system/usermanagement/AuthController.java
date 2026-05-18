00140313683d7f0046c0e08c819083d41c6d8f6e
IT25100010 <IT25100010@my.sliit.lk>
2026-05-19 00:25:10 +0530

    update AuthController

--- src/main/java/com/restaurant/reservation_system/usermanagement/AuthController.java
+++ src/main/java/com/restaurant/reservation_system/usermanagement/AuthController.java
@@ -1,6 +1,10 @@
 package com.restaurant.reservation_system.usermanagement;
 
+import com.restaurant.reservation_system.adminmanagement.Admin;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -15,14 +19,47 @@ public class AuthController {
         this.authService = authService;
         this.customerService = customerService;
     }
+
     @PostMapping("/register")
     public Customer register(@RequestBody Customer customer) {
         return customerService.registerCustomer(customer);
     }
 
     @PostMapping("/login")
-    public Customer login(@RequestBody LoginRequest loginRequest) {
-        return authService.login(loginRequest.getEmail(), loginRequest.getPassword());
-    }
+    public Map<String, Object> login(@RequestBody LoginRequest loginRequest) {
 
+        Object user = authService.login(
+                loginRequest.getEmail(),
+                loginRequest.getPassword()
+        );
+
+        Map<String, Object> response = new HashMap<>();
+
+        if (user instanceof Admin) {
+
+            Admin admin = (Admin) user;
+
+            response.put("status", "success");
+            response.put("role", "ADMIN");
+            response.put("redirect", "admin-dashboard.jsp");
+            response.put("admin", admin);
+
+        } else if (user instanceof Customer) {
+
+            Customer customer = (Customer) user;
+
+            response.put("status", "success");
+            response.put("role", "CUSTOMER");
+            response.put("redirect", "home.jsp");
+            response.put("customer", customer);
+
+        } else {
+
+            response.put("status", "failed");
+            response.put("message", "Invalid email or password");
+
+        }
+
+        return response;
+    }
 }
