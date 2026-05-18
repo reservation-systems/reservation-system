26d51cb8aaa125617cc5270417705cd62b469c7b
IT25100010 <IT25100010@my.sliit.lk>
2026-05-19 00:18:14 +0530

    Getters

--- src/main/java/com/restaurant/reservation_system/usermanagement/LoginRequest.java
+++ src/main/java/com/restaurant/reservation_system/usermanagement/LoginRequest.java
@@ -5,4 +5,8 @@ public class LoginRequest {
     private String email;
     private String password;
 
+    public String getEmail() { return email; }
+
+    public String getPassword() { return password; }
+
 }
